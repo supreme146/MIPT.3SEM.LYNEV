de068c143e33ea422f95b10a0dc9aaca5039e249
supreme146 <sdima96@gmail.com>
2015-09-24 00:16:53 +0300

    fuck all mac os

---
+++ cat.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+	int size;
+    int fileid;
+    char buf;
+    char buf2;
+    int pipefd[2];
+    pid_t cpid;
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <string>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    if (pipe(pipefd) == -1)
+    {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    cpid = fork();
+	if (cpid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if (cpid == 0)
+    {
+        fileid = open( argv[1], O_RDONLY);
+        close(pipefd[0]);
+	//size = 0;
+	size = read(fileid, &buf2, 4096);
+        while ( size > 0)
+	{
+        write(pipefd[1], &buf2, size);
+	size = read(fileid, &buf2, 4096);
+	}
+        close(pipefd[1]);
+        exit(EXIT_SUCCESS);
+    }else
+    {
+        close(pipefd[1]);
+	//size = 0;
+	size = read(pipefd[0], &buf, 4096);
+        while ( size > 0)
+	{
+	write(STDOUT_FILENO, &buf, size);
+	size = read(pipefd[0], &buf, 4096);
+	}
+        close(pipefd[0]);
+        exit(EXIT_SUCCESS);
+    }
+return 0;
+}
---
+++ m.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main( argc, char * argv[])
+{
+	int i;
+	int x;
+	while ((i = x) 
